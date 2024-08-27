515820a6ccbe378e11684a1349d17d00d2b87802
Mnahil-Naseer <mnahilnaseer@gmail.com>
2024-08-28 01:35:39 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Lottie from 'react-lottie';
 import animationData from './components/lottie4.json';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     const storedMode = localStorage.getItem('darkMode');
